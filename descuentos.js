f43fe56a6bd8e98cc407e98c1c1a5988a69649a1
Wilmer Coyote <wilmercoyote1113@gmail.com>
2022-10-09 21:50:20 -0600

    Se implementó el sistema de códigos de descuento

--- descuentos.js
+++ descuentos.js
@@ -3,23 +3,49 @@ const inPrice = document.getElementById('price');
 const inDiscount = document.getElementById('discount');
 const pResult = document.getElementById('result');
 
+// let cupones = {1001:10,1002:15,1003:20,1004:25,1005:18,1006:12};
+
+let couponList = [];
+couponList.push({
+    name: 'A10',
+    discount: 10
+});
+couponList.push({
+    name: 'A11',
+    discount: 15
+});
+couponList.push({
+    name: 'A12',
+    discount: 20
+});
+
 btn.addEventListener('click', calculateDiscount);
 
 function calculateDiscount () {
     const price = Number(inPrice.value);
-    const discount = Number(inDiscount.value);
+    const coupon = inDiscount.value;
 
-    if (!price || !discount) {
-        pResult.innerText = '¡¡Porfavor, introduce los valores correntamente!!';
+    function isCouponInArray (couponElement) {
+        return couponElement.name == coupon;
+    }
+
+    const couponInArray = couponList.find(isCouponInArray);
+
+    if (!price) {
+        pResult.innerText = '¡¡Porfavor, introduce un precio!!';
+        return;
+    }
+    if (!inDiscount.value) {
+        pResult.innerText = 'Usted no posee ningún código de descuento, su precio sigue siendo: Q' + price;
+        return;
+    }
+    if (!couponInArray) {
+        pResult.innerHTML = '¡¡El código de descuento no es valido!!</br>Se le aplicará el precio normal: Q' + price;
         return;
     }
 
-    if (discount > 100) {
-        pResult.innerText = '¡¡El descuento no puede ser de mas del 100%!!';
-        return;
-    }
-
+    let discount = couponInArray.discount;
     let newPrice = price - (price * (discount/100))
 
-    pResult.innerText = 'El nuevo precio con el descuento es de: Q' + newPrice;
+    pResult.innerHTML = 'El código utilizado le da un descuento del ' + discount + '%</br>El nuevo precio con el descuento es de: Q' + newPrice;
 }
